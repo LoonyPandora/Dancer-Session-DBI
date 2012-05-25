c0d4cfbbf67e7117089f32822058191676fd094d
James Aitken <loonypandora@gmail.com>
2012-05-26 00:55:09 +0100

    A simple test app

---
+++ test_app/bin/app.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use lib '/Library/WebServer/Documents/Dancer-Session-DBI/lib';
+
+use Dancer;
+use test_app;
+dance;
---
+++ test_app/lib/test_app.pm
@@ -0,0 +1,10 @@
+package test_app;
+use Dancer ':syntax';
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+true;
